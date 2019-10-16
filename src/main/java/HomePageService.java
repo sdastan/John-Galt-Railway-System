1dad67e0b0e3a0eec52c6176efc4c94c8303f672
Keno-AI <kenessary.myrzakul@nu.edu.kz>
2019-10-16 13:35:56 +0600

    Some minor commitments like adding new directories

--- src/main/java/HomePageService.java
+++ src/main/java/HomePageService.java
@@ -1,12 +1,13 @@
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
 @Path("/index")
 public class HomePageService {
     @GET
-    public Response getItems () {
-        String r = " Welcome to John Galt";
-        return Response. ok (r ). build ();
+    @Produces("text/html")
+    public String getHtml() {
+        return "<html lang=\"en\"><body><h1>Hello, World!!</h1></body></html>";
     }
 }
